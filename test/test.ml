ebb7be54ea09f974b50e32d9c3ffa5237e1fe285
Vincent Bernardoff <vb@luminar.eu.org>
2020-01-17 12:55:31 +0100

    test: use envvar

--- test/test.ml
+++ test/test.ml
@@ -2,22 +2,6 @@ open Core
 open Async
 open Bmex_rest
 
-module Cfg = struct
-  type cfg = {
-    key: string ;
-    secret: string ;
-    passphrase: string [@default ""];
-    quote: (string * int) list [@default []];
-  } [@@deriving sexp]
-
-  type t = (string * cfg) list [@@deriving sexp]
-end
-
-let default_cfg = Filename.concat (Option.value_exn (Sys.getenv "HOME")) ".virtu"
-let cfg =
-  List.Assoc.find_exn ~equal:String.equal
-    (Sexplib.Sexp.load_sexp_conv_exn default_cfg Cfg.t_of_sexp) "BMEXT"
-
 let wrap_request
     ?(timeout=Time.Span.of_int_sec 5)
     ?(speed=`Quick) n service =
@@ -35,18 +19,22 @@ let wrap_request_light
     Deferred.ignore
   end
 
+let key, secret =
+  match String.split ~on:':' (Sys.getenv_exn "TOKEN_BMEXT") with
+  | [key; secret] -> key, secret
+  | _ -> assert false
+
 let orderCycle () =
   let clOrdID = Uuidm.create `V4 in
   let price = 6000. in
   let o = createOrder
       ~price ~symbol:"XBTUSD"
       ~orderQty:1 ~ordType:Limit clOrdID in
-  submit ~testnet:true
-    ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret [o] >>= fun ords ->
+  submit ~testnet:true ~key ~secret [o] >>= fun ords ->
   let bmxO = List.hd_exn ords in
   let amds = [createAmend ~orderID:bmxO.orderID ~price:(price +. 100.) ()] in
-  amend ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret amds >>= fun _ ->
-  cancel ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret ~clOrdIDs:[clOrdID] ()
+  amend ~testnet:true ~key ~secret amds >>= fun _ ->
+  cancel ~testnet:true ~key ~secret ~clOrdIDs:[clOrdID] ()
 
 let date =
   Option.value_exn (Ptime.of_date_time ((2019,08,23), ((0,0,0),0)))
@@ -61,24 +49,24 @@ let rest = [
   Alcotest_async.test_case "trades" `Quick (fun () ->
       raise_on_error (fun () -> trades "XBTUSD")) ;
   Alcotest_async.test_case "tradeHistory" `Quick (fun () ->
-      raise_on_error (tradeHistory ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret)) ;
+      raise_on_error (tradeHistory ~testnet:true ~key ~secret)) ;
   Alcotest_async.test_case "walletHistory" `Quick (fun () ->
-      raise_on_error (walletHistory ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret)) ;
+      raise_on_error (walletHistory ~testnet:true ~key ~secret)) ;
   Alcotest_async.test_case "walletSummary" `Quick (fun () ->
-      raise_on_error (walletSummary ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret)) ;
+      raise_on_error (walletSummary ~testnet:true ~key ~secret)) ;
   Alcotest_async.test_case "wallet" `Quick (fun () ->
-      raise_on_error (wallet ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret)) ;
+      raise_on_error (wallet ~testnet:true ~key ~secret)) ;
   Alcotest_async.test_case "executionHistory" `Quick (fun () ->
-      raise_on_error (executionHistory ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret ~symbol:"XBTUSD" ~ts:date)) ;
+      raise_on_error (executionHistory ~testnet:true ~key ~secret ~symbol:"XBTUSD" ~ts:date)) ;
   Alcotest_async.test_case "positions" `Quick (fun () ->
-      raise_on_error (positions ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret)) ;
+      raise_on_error (positions ~testnet:true ~key ~secret)) ;
   Alcotest_async.test_case "openOrders" `Quick (fun () ->
-      raise_on_error (openOrders ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret)) ;
+      raise_on_error (openOrders ~testnet:true ~key ~secret)) ;
   Alcotest_async.test_case "orderCycle" `Quick (fun () -> raise_on_error orderCycle) ;
   Alcotest_async.test_case "deleteAll" `Quick (fun () ->
-      raise_on_error (cancelAll ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret)) ;
+      raise_on_error (cancelAll ~testnet:true ~key ~secret)) ;
   Alcotest_async.test_case "cancelAllAfter" `Quick (fun () ->
-      raise_on_error (fun () -> cancelAllAfter ~testnet:true ~key:cfg.Cfg.key ~secret:cfg.Cfg.secret (Time_ns.Span.of_int_sec 10))) ;
+      raise_on_error (fun () -> cancelAllAfter ~testnet:true ~key ~secret (Time_ns.Span.of_int_sec 10))) ;
 ]
 
 let () =
