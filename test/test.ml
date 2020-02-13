90f9cc632ba223179c1a815881f5459b993bf3d1
Vincent Bernardoff <vb@luminar.eu.org>
2020-02-13 14:47:39 +0100

    test: fix compilation (alcotest)

--- test/test.ml
+++ test/test.ml
@@ -1,11 +1,12 @@
 open Core
 open Async
 open Bmex_rest
+open Alcotest_async
 
 let wrap_request
     ?(timeout=Time.Span.of_int_sec 5)
     ?(speed=`Quick) n service =
-  Alcotest_async.test_case ~timeout n speed begin fun () ->
+  test_case ~timeout n speed begin fun () ->
     (Fastrest.request service) |>
     Deferred.Or_error.ignore_m |>
     Deferred.Or_error.ok_exn
@@ -14,7 +15,7 @@ let wrap_request
 let wrap_request_light
     ?(timeout=Time.Span.of_int_sec 5)
     ?(speed=`Quick) n f =
-  Alcotest_async.test_case ~timeout n speed begin fun () ->
+  test_case ~timeout n speed begin fun () ->
     f () |>
     Deferred.ignore_m
   end
@@ -42,36 +43,38 @@ let date =
 let raise_on_error f = Deferred.ignore_m (f ())
 
 let rest = [
-  Alcotest_async.test_case "instruments" `Quick (fun () ->
+  test_case "instruments" `Quick (fun () ->
       raise_on_error (fun () -> activeInstruments ())) ;
-  Alcotest_async.test_case "instrumentsAndIndices" `Quick (fun () ->
+  test_case "instrumentsAndIndices" `Quick (fun () ->
       raise_on_error (fun () -> activeAndIndices ())) ;
-  Alcotest_async.test_case "trades" `Quick (fun () ->
+  test_case "trades" `Quick (fun () ->
       raise_on_error (fun () -> trades "XBTUSD")) ;
-  Alcotest_async.test_case "tradeHistory" `Quick (fun () ->
+  test_case "tradeHistory" `Quick (fun () ->
       raise_on_error (tradeHistory ~testnet:true ~key ~secret)) ;
-  Alcotest_async.test_case "walletHistory" `Quick (fun () ->
+  test_case "walletHistory" `Quick (fun () ->
       raise_on_error (walletHistory ~testnet:true ~key ~secret)) ;
-  Alcotest_async.test_case "walletSummary" `Quick (fun () ->
+  test_case "walletSummary" `Quick (fun () ->
       raise_on_error (walletSummary ~testnet:true ~key ~secret)) ;
-  Alcotest_async.test_case "wallet" `Quick (fun () ->
+  test_case "wallet" `Quick (fun () ->
       raise_on_error (wallet ~testnet:true ~key ~secret)) ;
-  Alcotest_async.test_case "executionHistory" `Quick (fun () ->
+  test_case "executionHistory" `Quick (fun () ->
       raise_on_error (executionHistory ~testnet:true ~key ~secret ~symbol:"XBTUSD" ~ts:date)) ;
-  Alcotest_async.test_case "positions" `Quick (fun () ->
+  test_case "positions" `Quick (fun () ->
       raise_on_error (positions ~testnet:true ~key ~secret)) ;
-  Alcotest_async.test_case "openOrders" `Quick (fun () ->
+  test_case "openOrders" `Quick (fun () ->
       raise_on_error (openOrders ~testnet:true ~key ~secret)) ;
-  Alcotest_async.test_case "orderCycle" `Quick (fun () -> raise_on_error orderCycle) ;
-  Alcotest_async.test_case "deleteAll" `Quick (fun () ->
+  test_case "orderCycle" `Quick (fun () -> raise_on_error orderCycle) ;
+  test_case "deleteAll" `Quick (fun () ->
       raise_on_error (cancelAll ~testnet:true ~key ~secret)) ;
-  Alcotest_async.test_case "cancelAllAfter" `Quick (fun () ->
+  test_case "cancelAllAfter" `Quick (fun () ->
       raise_on_error (fun () -> cancelAllAfter ~testnet:true ~key ~secret (Time_ns.Span.of_int_sec 10))) ;
 ]
 
-let () =
-  Logs.set_reporter (Logs_async_reporter.reporter ()) ;
-  Logs.set_level (Some Debug) ;
-  Alcotest.run "bmex" [
+let main () =
+  run "bmex" [
     "rest", rest ;
   ]
+
+let () =
+  don't_wait_for (main ()) ;
+  never_returns (Scheduler.go ())
